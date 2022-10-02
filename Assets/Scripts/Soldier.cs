a48ec80cf780779288eac86b3ad3c1d348ca777b
barozinugroho <barozinugroho@gmail.com>
2022-10-02 18:20:24 +0700

    FIX: rotation soldier

--- Assets/Scripts/Soldier.cs
+++ Assets/Scripts/Soldier.cs
@@ -1,6 +1,9 @@
 using System.Collections;
+using System.Numerics;
 using UnityEditor.Timeline.Actions;
 using UnityEngine;
+using Quaternion = UnityEngine.Quaternion;
+using Vector3 = UnityEngine.Vector3;
 
 public class Soldier : MonoBehaviour
 {
@@ -39,18 +42,31 @@ public class Soldier : MonoBehaviour
                 if (!GameManager.instance.isBallOccupied)
                 {
                     //find ball
-                    //transform.LookAt(GameManager.instance.spawnedBall.transform);
-                    transform.SetPositionAndRotation(transform.position, Quaternion.LookRotation(GameManager.instance.spawnedBall.transform.position));
+                    /*Vector3 direction = GameManager.instance.spawnedBall.transform.position - transform.position;
+                    direction.y = 0f;
+                    Quaternion rotateTOTarget = Quaternion.LookRotation(direction);
+                    transform.rotation = Quaternion.Lerp(transform.rotation, rotateTOTarget, 3f*Time.deltaTime);*/
+
+                    Vector3 direction = GameManager.instance.spawnedBall.transform.position - transform.position;
+                    direction.y = 0f;
+                    Vector3 rotateToTarget = Vector3.RotateTowards(transform.forward, direction, 3f * Time.deltaTime, 0.0f);
+                    transform.rotation = Quaternion.LookRotation(rotateToTarget);
                 }
                 else
                 {
                     if (transform.GetChild(0).CompareTag("Ball"))
                     {
-                        transform.LookAt(GameManager.instance.defender.land.GetComponent<Land>().gate.transform);
+                        Vector3 direction = new Vector3(GameManager.instance.defender.land.GetComponent<Land>().gate.transform.position.x,
+                            this.transform.position.y,
+                            GameManager.instance.defender.land.GetComponent<Land>().gate.transform.position.z);
+                        transform.LookAt(direction);
                     }
                     else
                     {
-                        transform.LookAt(GameManager.instance.defender.land.transform);
+                        Vector3 direction = new Vector3(GameManager.instance.defender.land.GetComponent<Land>().fence.transform.position.x,
+                            this.transform.position.y,
+                            GameManager.instance.defender.land.GetComponent<Land>().fence.transform.position.z);
+                        transform.LookAt(direction);
                     }
                 }
             }
